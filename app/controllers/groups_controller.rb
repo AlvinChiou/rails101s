56ce3cde7019c57d6e7384f094a22b5ed7711a0e
AlvinChiu <alvinhsian@gmail.com>
2015-08-09 15:59:02 +0800

    第四章：手動實作出有 CRUD 功能的討論版(前置作業)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,8 +2,32 @@ class GroupsController < ApplicationController
 
   def index
     flash[:notice] = "早安！"
-    flash[:alert] = "晚安！該睡了！"
-    flash[:warning] = "測試Warning"
+    # flash[:alert] = "晚安！該睡了！"
+    # flash[:warning] = "測試Warning"
+  end
+
+  def show
+    
+  end
+
+  def new
+    
+  end
+
+  def edit
+    
+  end
+
+  def create
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
   end
 
 end
