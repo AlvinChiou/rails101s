de111117df66a300ca1cf8905b87262e6691d729
AlvinChiu <alvinhsian@gmail.com>
2015-08-11 01:26:51 +0800

    [feature] 建立 Create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,7 +21,13 @@ class GroupsController < ApplicationController
   end
 
   def create
-    
+    @group = Group.create(group_params)
+
+    if @group.save
+      redirect_to groups_path, notice: '新增群組成功！'
+    else
+      render :new
+    end
   end
 
   def update
@@ -30,6 +36,11 @@ class GroupsController < ApplicationController
 
   def destroy
     
+  end
+# 以下為擴充method
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
   end
 
 end
