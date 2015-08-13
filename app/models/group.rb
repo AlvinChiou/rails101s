33bb065fa9393bd7b3bc98b31bbc6ad11d39dd25
AlvinChiu <alvinhsian@gmail.com>
2015-08-13 22:52:27 +0800

    不用 Scaffold 實作出有 CRUD 功能的 group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ActiveRecord::Base
-  # validates :title, presence: true
-  validates :title, presence: {message: '幹！標題是不會寫喔？'}
+  validates :title, presence: true
+  # validates :title, presence: {message: '幹！標題是不會寫喔？'}
 end
