61d1d57cb8bc481743de72998b6e3f4f925cec8d
AlvinChiu <alvinhsian@gmail.com>
2015-08-19 01:22:48 +0800

    能在 group 的 show 頁面顯示所擁有的 post，目前post還沒有資料。故畫面一片空白

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
   validates :title, presence: true
   # validates :title, presence: {message: '幹！標題是不會寫喔？'}
-  has_many :post
+  has_many :posts
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  resources :groups
-
   root 'groups#index'
+
+  resources :groups do
+    resources :post
+  end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
