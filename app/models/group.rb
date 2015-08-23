4f1b963ae2def55aee7a395338e515ee23be8c62
AlvinChiu <alvinhsian@gmail.com>
2015-08-23 17:21:53 +0800

    使用者功能 整合進 group / post 裡面 (作者機制) - 設定 user 跟 group 之間的資料庫關聯 ( database relationship )

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ActiveRecord::Base
   validates :title, presence: true
   # validates :title, presence: {message: '幹！標題是不會寫喔？'}
   has_many :posts
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
 end
