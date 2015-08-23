dd8e1e6e742fab0e2ab651c26a3f70c7de24deeb
AlvinChiu <alvinhsian@gmail.com>
2015-08-24 01:02:14 +0800

    修改 view => 只有作者才會出現 edit / delete 按鈕

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,10 @@ class Group < ActiveRecord::Base
   # validates :title, presence: {message: '幹！標題是不會寫喔？'}
   has_many :posts
   belongs_to :owner, class_name: "User", foreign_key: :user_id
+
+  # 只有作者才會出現 edit / delete 按鈕
+  def editable_by?(user)
+    user && user == owner
+  end
+
 end
