9a146edb2ca92064d0cd7e67cbe0f22557e94513
AlvinChiu <alvinhsian@gmail.com>
2015-08-24 23:58:20 +0800

    只有作者才能有 post 的修改/刪除權限

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,16 +8,20 @@ class PostsController < ApplicationController
 
   # before_action: find_group, except: [:show, :index] 或者使用 except,排除某些 action 不執行
 
+  before_action :current_user_posts, only:[:edit, :update, :destroy]
+
   def new
     @post = @group.posts.new
   end
 
   def edit
-    @post = @group.posts.find(params[:id])
+    # @post = @group.posts.find(params[:id])
   end
 
   def create
     @post = @group.posts.build(post_params)
+
+    @post.author = current_user
 
     if @post.save
       redirect_to group_path(@group), notice: "新增文章成功！"
@@ -27,7 +31,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = @group.posts.find(params[:id])
+    # @post = @group.posts.find(params[:id])
 
     if @post.update(post_params)
       redirect_to group_path(@group), notice:"文章修改成功！"
@@ -38,7 +42,7 @@ class PostsController < ApplicationController
 
   def destroy
     #先驗證這個 group 裡面是否有這個 post，如果有就刪除
-    @post = @group.posts.find(params[:id])
+  #  @post = @group.posts.find(params[:id])
 
     @post.destroy
     redirect_to group_path(@group), alert:"文章已刪除"
@@ -54,4 +58,8 @@ class PostsController < ApplicationController
     @group = Group.find(params[:group_id])
   end
 
+  private
+  def current_user_posts
+    @post = current_user.posts.find(params[:id])
+  end
 end
