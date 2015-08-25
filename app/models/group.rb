d75c7442f883a0567bee9a1d59c1eb83d1baf888
AlvinChiu <alvinhsian@gmail.com>
2015-08-26 00:07:29 +0800

    建立 討論版成員 ( group_user ) 設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ActiveRecord::Base
   validates :title, presence: true
   # validates :title, presence: {message: '幹！標題是不會寫喔？'}
   has_many :posts
+  has_many :group_users
+  has_many :members, through: :group_users, source: :user
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
   # 只有作者才會出現 edit / delete 按鈕
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   has_many :groups
+  has_many :group_users
+  has_many :participated_groups, through: :group_users, source: :group
   has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
