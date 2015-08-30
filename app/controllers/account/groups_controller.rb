d12ecc21d9f92e4ff14b0dd09aa7e0c3cafae94c
AlvinChiu <alvinhsian@gmail.com>
2015-08-30 12:11:19 +0800

    修正Account::GroupsController

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -2,7 +2,6 @@ class Account::GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.posts.order("updated_at DESC")
-    #@posts = current_user.participated_posts
+    @groups = current_user.participated_groups
   end
 end
