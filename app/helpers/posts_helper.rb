761e242906e505f66db52ff9853c7bb658c2102c
AlvinChiu <alvinhsian@gmail.com>
2015-08-31 00:48:24 +0800

    自己撰寫的 helper 包裝 html

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+  # Use in view: <%= render_post_content(post, number) %>
+  def render_post_content(post, int)
+    truncate(simple_format(post.content), length: int)
+  end
 end
