040036b3afa9616027d588f63639a65a641c5473
AlvinChiu <alvinhsian@gmail.com>
2015-08-30 12:29:00 +0800

    bug 解決 => 當 group 刪除時，所屬的 posts 也要跟著刪除

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveRecord::Base
   validates :title, presence: true
   # validates :title, presence: {message: '幹！標題是不會寫喔？'}
-  has_many :posts
+  has_many :posts, dependent: :destroy # 當 group 刪除的時候，他所屬的 posts 也要跟著刪
   has_many :group_users
   has_many :members, through: :group_users, source: :user
   belongs_to :owner, class_name: "User", foreign_key: :user_id
