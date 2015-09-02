caeeb0e065c9df860b65132d9d5e8f2d44641eaf
AlvinChiu <alvinhsian@gmail.com>
2015-09-03 00:14:32 +0800

    使用 scope 整理 query

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,11 @@ class Post < ActiveRecord::Base
   # 只要設定好 counter_cache: :posts_count <== (你剛剛建立的欄位名稱)
   # 只要 post 有 create 跟 destroy 的動作，就會自動在 posts_count 欄位 +1 跟 -1
   belongs_to :group, counter_cache: :post_count
+  belongs_to :author, class_name: "User", foreign_key: :user_id
   validates :content, presence: true
 
-  belongs_to :author, class_name: "User", foreign_key: :user_id
+  # 使用 scope 整理 query
+  scope :recent, -> {order("updated_at DESC")}
 
   def editable_by?(user)
     user && user == author
